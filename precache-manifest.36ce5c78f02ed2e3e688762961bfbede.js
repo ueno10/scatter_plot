f0f842a8fc82ba50412ca19f6a6408b6758039fe
ueno10 <uenomizuki.nu@gmail.com>
2020-07-23 23:05:01 +0900

    Updates

---
+++ precache-manifest.36ce5c78f02ed2e3e688762961bfbede.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "824422d6760c891d97597afb16babae0",
+    "url": "/scatter_plot/index.html"
+  },
+  {
+    "revision": "ebe4a3b9e31b59ff0096",
+    "url": "/scatter_plot/static/js/2.80210044.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scatter_plot/static/js/2.80210044.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac3ff8fadd3e4bab70d0",
+    "url": "/scatter_plot/static/js/main.6a337ae4.chunk.js"
+  },
+  {
+    "revision": "18e853a0973eba98e893",
+    "url": "/scatter_plot/static/js/runtime-main.b54f4f1e.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/scatter_plot/precache-manifest.36ce5c78f02ed2e3e688762961bfbede.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/scatter_plot/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
