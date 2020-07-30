93406a012723eb874a92eb21e4e6da1ce0fbcb89
ueno10 <uenomizuki.nu@gmail.com>
2020-07-31 03:00:31 +0900

    Updates

---
+++ precache-manifest.e213436cb7fa7c364ec3b59fbf2dc7b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd38de1abbba8eb548628b76d9fb794e",
+    "url": "/scatter_plot/index.html"
+  },
+  {
+    "revision": "cb2fdc163646c6f6feda",
+    "url": "/scatter_plot/static/js/2.97a70f3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scatter_plot/static/js/2.97a70f3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3db0ebf7d72eb015ff9d",
+    "url": "/scatter_plot/static/js/main.03678f00.chunk.js"
+  },
+  {
+    "revision": "18e853a0973eba98e893",
+    "url": "/scatter_plot/static/js/runtime-main.b54f4f1e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/scatter_plot/precache-manifest.49e7e6a5be648225ee49727129a66608.js"
+  "/scatter_plot/precache-manifest.e213436cb7fa7c364ec3b59fbf2dc7b7.js"
 );
 
 self.addEventListener('message', (event) => {
